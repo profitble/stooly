2e4edd59d8a33d8aa6f1a0f3eb67090e227d8ddf
maxta <maxta334@gmail.com>
2025-07-23 02:54:39 -0700

    Push latest changes

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,5 @@
-console.log('[FILE LOADED: app.config.ts]');
 import type { ExpoConfig } from 'expo/config';
-
-console.log('[CONFIG_ENV] Process env loaded:', typeof process.env);
-console.log('[CONFIG_ENV] iOS RC key present:', !!process.env.EXPO_PUBLIC_REVENUECAT_API_KEY_IOS);
-console.log('[CONFIG_ENV] API base URL present:', !!process.env.EXPO_PUBLIC_API_BASE_URL);
-console.log('[CONFIG_ENV] Supabase key present:', !!process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY);
-
 const revenueCatApiKeyIos = process.env.EXPO_PUBLIC_REVENUECAT_API_KEY_IOS;
-console.log('[CONFIG_RC] Revenue Cat iOS key retrieved:', !!revenueCatApiKeyIos);
 
 const config: ExpoConfig = {
   name: "Stooly",
@@ -82,8 +74,5 @@ const config: ExpoConfig = {
   },
   owner: "maxta"
 };
-
-console.log('[CONFIG_EXPORT] Config object created, extra keys:', Object.keys(config.extra || {}));
-console.log('[CONFIG_EXPORT] Exporting config with RC key:', !!config.extra?.revenueCatApiKeyIos);
 
 export default config;
