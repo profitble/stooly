737e2078b441697e0ba62b55ec39803594f72102
maxta <maxta334@gmail.com>
2025-07-23 20:10:49 -0700

    Push latest changes

--- app.config.ts
+++ app.config.ts
@@ -18,7 +18,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: "com.maxta.poop",
-    buildNumber: "22",
+    buildNumber: "23",
     infoPlist: {
       CFBundleAllowMixedLocalizations: true,
       ITSAppUsesNonExemptEncryption: false,
